47dfaadca3008dba7ec474809d31be5dba9e8080
coder_pikachu <coderpika@gmail.com>
2020-05-01 10:57:20 +0800

    initial class writing

--- lib/table/student.dart
+++ lib/table/student.dart
@@ -10,4 +10,28 @@ class Student {
   int telephone;
   int flatId;
   int dormitoryId;
+  Student.fromDb(Map<String, dynamic> parsedJson)
+      : id = parsedJson['id'],
+        name = parsedJson['name'],
+        gender = parsedJson['gender'] ? Gender.M : Gender.F,
+        nation = parsedJson['nation'],
+        specialty = parsedJson['specialty'],
+        classId = parsedJson['classId'],
+        telephone = parsedJson['telephone'],
+        flatId = parsedJson['flatId'],
+        dormitoryId = parsedJson['dormitoryId'];
+
+  Map<String, dynamic> toMapForDb() {
+    return <String, dynamic>{
+      'id': id,
+      'name': name,
+      'gender': gender == Gender.M,
+      'nation': nation,
+      'specialty': specialty,
+      'classId': classId,
+      'telephone': telephone,
+      'flatId': flatId,
+      'dormitoryId': dormitoryId,
+    };
+  }
 }
